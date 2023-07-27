2366fe0e639c329b34acdf2a06e6b5add75c3cce
Adarlan Teixeira <adarlan18@gmail.com>
2023-07-27 13:53:21 -0300

    Update deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+GitUrl="https://github.com/adarlan/traefik-swarm-deployment.git"
+GitBranch="main"
+DeployFile="swarm-compose.yaml"
+StackName="default"
+
+SwarmActive=$(docker info | grep "Swarm: active") || SwarmInit=$(docker swarm init)
+
+echo -e "\n$(date)"
+cd ~
+Updated=0
+if [ ! -d swarm-deployment ]; then
+    git clone $GitUrl swarm-deployment
+    cd swarm-deployment
+    git checkout $GitBranch
+    Updated=1
+else
+    cd swarm-deployment
+    git checkout $GitBranch
+    git fetch
+    if [ ! $(git rev-list HEAD..origin/$GitBranch --count) -eq 0 ]; then
+        git pull
+        Updated=1
+    fi
+fi
+if [ $Updated -eq 1 ]; then
+    docker stack deploy --compose-file $DeployFile $StackName
+fi
