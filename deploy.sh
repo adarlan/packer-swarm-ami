b87c70e0f08c15fa6fe4120469cc9c55369e1d86
Adarlan Teixeira <adarlan18@gmail.com>
2023-07-28 20:41:41 -0300

    Fixed jq command

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -e
 
+echo -e "\n$(date)"
+
 ParameterName=swarm_deployment_config
 ParameterValue=$(aws ssm get-parameter --name $ParameterName --query "Parameter.Value" | jq --monochrome-output fromjson)
 
-GitUrl=$(echo $ParameterValue | jq '.GitUrl')
-GitBranch=$(echo $ParameterValue | jq '.GitBranch')
-DeployFile=$(echo $ParameterValue | jq '.DeployFile')
-StackName=$(echo $ParameterValue | jq '.StackName')
+GitUrl=$(echo $ParameterValue | jq -r '.GitUrl')
+GitBranch=$(echo $ParameterValue | jq -r '.GitBranch')
+DeployFile=$(echo $ParameterValue | jq -r '.DeployFile')
+StackName=$(echo $ParameterValue | jq -r '.StackName')
 
 SwarmActive=$(docker info | grep "Swarm: active") || SwarmInit=$(docker swarm init)
 
-echo -e "\n$(date)"
 cd ~
 RepoUpdated=false
 if [ ! -d swarm-deployment ]; then
