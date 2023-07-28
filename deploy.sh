ae3bce52b7c8ce303105442d7e59872baa8cd1d5
Adarlan Teixeira <adarlan18@gmail.com>
2023-07-28 20:14:24 -0300

    Add parameter reading

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 set -e
 
-GitUrl="https://github.com/adarlan/traefik-swarm-deployment.git"
-GitBranch="main"
-DeployFile="swarm-compose.yaml"
-StackName="default"
+ParameterName=swarm_deployment_config
+ParameterValue=$(aws ssm get-parameter --name $ParameterName --query "Parameter.Value" | jq --monochrome-output fromjson)
+
+GitUrl=$(echo $ParameterValue | jq '.GitUrl')
+GitBranch=$(echo $ParameterValue | jq '.GitBranch')
+DeployFile=$(echo $ParameterValue | jq '.DeployFile')
+StackName=$(echo $ParameterValue | jq '.StackName')
 
 SwarmActive=$(docker info | grep "Swarm: active") || SwarmInit=$(docker swarm init)
 
 echo -e "\n$(date)"
 cd ~
-Updated=0
+RepoUpdated=false
 if [ ! -d swarm-deployment ]; then
     git clone $GitUrl swarm-deployment
     cd swarm-deployment
     git checkout $GitBranch
-    Updated=1
+    RepoUpdated=true
 else
     cd swarm-deployment
     git checkout $GitBranch
     git fetch
     if [ ! $(git rev-list HEAD..origin/$GitBranch --count) -eq 0 ]; then
         git pull
-        Updated=1
+        RepoUpdated=true
     fi
 fi
-if [ $Updated -eq 1 ]; then
+if [ $RepoUpdated = true ]; then
     docker stack deploy --prune --compose-file $DeployFile $StackName
     docker system prune --all --force
 fi
