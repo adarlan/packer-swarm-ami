5132f66f7fecb1435eb765e326b03e9bf4e2a13b
Adarlan Teixeira <adarlan18@gmail.com>
2023-07-29 00:06:45 -0300

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,33 +3,44 @@ set -e
 
 echo -e "\n$(date)"
 
-ParameterName=swarm_deployment_config
-ParameterValue=$(aws ssm get-parameter --name $ParameterName --query "Parameter.Value" | jq --monochrome-output fromjson)
+echo "SSMParameterName: $SSMParameterName"
+SSMParameterValue=$(aws ssm get-parameter --with-decryption --name $SSMParameterName --query "Parameter.Value" | jq --monochrome-output fromjson)
+echo "SSMParameterValue: $SSMParameterValue"
 
-GitUrl=$(echo $ParameterValue | jq -r '.GitUrl')
-GitBranch=$(echo $ParameterValue | jq -r '.GitBranch')
-DeployFile=$(echo $ParameterValue | jq -r '.DeployFile')
-StackName=$(echo $ParameterValue | jq -r '.StackName')
+GitPlatform=$(echo $SSMParameterValue | jq -r '.git_platform')
+GitUrl=$(echo $SSMParameterValue | jq -r '.git_url')
+GitRef=$(echo $SSMParameterValue | jq -r '.git_ref')
+echo "GitPlatform: $GitPlatform"
+echo "GitUrl: $GitUrl"
+echo "GitRef: $GitRef"
 
-SwarmActive=$(docker info | grep "Swarm: active") || SwarmInit=$(docker swarm init)
+docker info | grep "Swarm: active" && SwarmActive=true || SwarmActive=false
+if [ $SwarmActive = true ]; then
+    echo "SwarmActive: true"
+else
+    echo "SwarmActive: false"
+    docker swarm init
+fi
 
 cd ~
 RepoUpdated=false
 if [ ! -d swarm-deployment ]; then
     git clone $GitUrl swarm-deployment
     cd swarm-deployment
-    git checkout $GitBranch
+    git checkout $GitRef
     RepoUpdated=true
 else
     cd swarm-deployment
-    git checkout $GitBranch
-    git fetch
-    if [ ! $(git rev-list HEAD..origin/$GitBranch --count) -eq 0 ]; then
+    git fetch --all --tags
+    git checkout $GitRef
+    if [ ! $(git rev-list HEAD..origin/$GitRef --count) -eq 0 ]; then
         git pull
         RepoUpdated=true
     fi
 fi
 if [ $RepoUpdated = true ]; then
+    DeployFile=swarm-compose.yaml
+    StackName=main
     docker stack deploy --prune --compose-file $DeployFile $StackName
     docker system prune --all --force
 fi
